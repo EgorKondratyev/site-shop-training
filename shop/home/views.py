5a6bd5d3026e0d57b5d7693e55ee696420f07e1e
Vladislav <dubovecvladislav0@gmail.com>
2023-02-01 17:47:57 +0300

    header without bootstrap

--- shop/home/views.py
+++ shop/home/views.py
@@ -117,7 +117,7 @@ class LoginUser(DataMixin, LoginView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context_base = super(LoginUser, self).get_context_data()
-        context = self.user_context_data(**context_base, title='Регистрация')
+        context = self.user_context_data(**context_base, title='Вход')
         return context
 
     def get_success_url(self):
